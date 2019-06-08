78e6f8919d44153901c79f720277b07d30e73326
Mike-Rynes <50651981+Mike-Rynes@users.noreply.github.com>
2019-06-09 09:01:41 +1000

    Add files via upload

---
+++ build/precache-manifest.e01600296b67b710661ad1599d7943a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d21160eebb2d24f0c262844146866d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "7e53de701ccd32bddb10",
+    "url": "/todo/static/css/main.623ac21f.chunk.css"
+  },
+  {
+    "revision": "8ab11477ee3a82a16876",
+    "url": "/todo/static/js/2.250409a9.chunk.js"
+  },
+  {
+    "revision": "7e53de701ccd32bddb10",
+    "url": "/todo/static/js/main.194f09c2.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "51b74ca34b084e92e11cf5882fd2c11f",
+    "url": "/todo/static/media/applogo.51b74ca3.png"
+  },
+  {
+    "revision": "487c4f8de8e1d8942439724590e80705",
+    "url": "/todo/static/media/viccleaners-logo.487c4f8d.png"
+  }
+]);
